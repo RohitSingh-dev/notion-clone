0d3dfa717bd1ff94c4115671578b85339e72449e
RohitSingh-dev <rohitsingh20082002@gmail.com>
2025-08-27 00:03:26 +0530

    Started on Sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,25 @@
 'use client'
 
-import { useUser } from "@clerk/nextjs"
+import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from "@clerk/nextjs"
 
 function Header() {
   const {user} = useUser();
   return (
-    <div>
+    <div className="flex items-center justify-between p-5">
       {user && (
-        <h1>{user?.firstName}</h1>
+        <h1 className="text-2xl">{user?.firstName}{`'s`} Space</h1>
       )}
+
+      {/* Breadcrumbs */}
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </div>
   )
 }
