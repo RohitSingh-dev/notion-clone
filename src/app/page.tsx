3e26d25328a242e8c0ce17d5bb1c02da44310336
RohitSingh-dev <rohitsingh20082002@gmail.com>
2025-09-08 00:00:20 +0530

    creating doc view page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { ArrowLeftCircle } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        <h1>Let&apos;s buid a AI productivity app</h1>
-        <Button>Click Me</Button>
-      </main>
-    </div>
+    <main className="flex space-x-2 items-center animate-pulse">
+      <ArrowLeftCircle className="w-12 h-12" />
+      <h1 className="font-bold">Get started with creating a New Document</h1>
+    </main>
   );
 }
---
+++ src/components/Document.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import { FormEvent, useEffect, useState, useTransition } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import React from "react";
+
+type DocumentProps = {
+  id: string; 
+};
+
+function Document({id}: DocumentProps) {
+  const [data, loading, error] = useDocumentData(doc(db, "documents", id));
+  const [input, setInput] = useState("");
+  const [isUpdating, startTransition] = useTransition();
+
+  useEffect(() => {
+    if (data) {
+      setInput(data.title);
+    }
+  }, [data]);
+
+  const updateTitle = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (input.trim()) {
+      startTransition(async () => {
+        await updateDoc(doc(db, "documents", id), { title: input, });
+      });
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <form onSubmit={updateTitle}>
+          {/*  update title...  */}
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
+
+          <Button disabled={isUpdating} type="submit">
+            {isUpdating ? "Updating..." : "Update"}
+          </Button>
+
+          {/*  IF  */}
+          {/*  isOwner && InviteUser, DeleteDocument */}
+        </form>
+      </div>
+
+      <div>
+        {/* ManageUsers */}
+
+        {/* Avatars */}
+      </div>
+
+      {/* Collaborate Editor */}
+    </div>
+  )
+}
+export default Document
